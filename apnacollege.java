7954b55fd5f4099f493c059177d81f5e7934e8ba
Tanisha <tanishasuhag127@gmail.com>
2025-03-20 09:46:34 +0530

    Initial commit

---
+++ apnacollege.java
@@ -0,0 +1,6 @@
+import java.util.*;
+public class apnacollege {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+marks=80
+if marks==80 or marks==90 or marks==95:
+    print("good job")
+if 80 in [80,90,95]:
+    print("good job")
